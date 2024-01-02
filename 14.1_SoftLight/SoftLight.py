f205763d6c66c70bcd2c652e869dd245593f89d8
Wink Saville <wink@saville.com>
2024-01-01 18:19:17 -0800

    feat: Use EMA filter
    This is "faster" and doesn't require an array of the moving average filter I
    had before. I may have something wrong but the statement to only update
    on "changes", which I want to do to minimize the print noise, causes the LED
    to "never" turn off when the pot is turned off. Adding the diff_clamp_to_0
    resolves the problem although there is probably a better way.
    
    Also, adding the delay between samples seems to make things "smoother",
    not sure why but feels that way to me.

--- 14.1_SoftLight/SoftLight.py
+++ 14.1_SoftLight/SoftLight.py
@@ -1,75 +1,59 @@
 from microbit import *
+import utime
 
-readings = [pin0.read_analog()] * 50
-readings_index: int = 0
-sum_readings: int = 0
-avg_as_float: float = 0.0
-av_round_up: float = 0.0
-cv: int = 0
-av: int = 0
 
-def print_readings():
-    """Print the current readings   """
-    global readings_index, readings, cv, av, av_round, sum_readings
-    print("readings_index: " + str(readings_index) +
-          " cv: " + str(cv) +
-          " sum: " + str(sum_readings) +
-          " avg_as_float: " + str(avg_as_float) +
-          " av_round_up: " + str(av_round_up) +
-          " av: " + str(av) +
-          " readings: " + str(readings))
+def filter_reading(prev_value: int, cur_value: int) -> int:
+    """Filter reading using EMA filter"""
+    EMA_alpha: float = 0.6
+    return (EMA_alpha * cur_value) + ((1 - EMA_alpha) * prev_value)
 
-def pin0_read_analog() -> int:
-    """Read the analog value of pin0
-    filter it with a moving average filter.
-    This is "OK" but not great it will still
-    wonder around a bit."""
-    global readings_index, readings, sum_readings, cv, av, av_round_up, avg_as_float
-
-    # Read the current value
+def pin0_read_analog(prev_value: int) -> int:
+    """Read the analog value of pin0 and filter"""
     cv = pin0.read_analog()
+    return filter_reading(prev_value, cv)
 
-    # Update the readings array
-    readings[readings_index] = cv
+def round_up(x: float) -> int:
+    """Round up a float to the nearest integer"""
+    return int(x + 0.5)
 
-    # increment the index and wrap around
-    readings_index = (readings_index + 1) % len(readings)
+# Trigger to update pin1
+diff_fuzz = 1.0
 
-    # Sum the readings
-    sum_readings = sum(readings)
+# Clamp pot_value to 0 if diff is >= diff_clamp_to_0
+diff_clamp_to_0 = 0.01
 
-    # Calculate the average, which is a float
-    avg_as_float = sum_readings / len(readings)
-
-    # Round up by 0.5
-    av_round_up = avg_as_float + 0.5
-
-    # Truncate to an int
-    av = int(av_round_up)
-
-    # return the average integer value
-    return av
-    
 # Read pot_value and make prev_pot_value different
 # so the first time through the loop we'll always
 # write a new value.
-pot_value: int = pin0_read_analog()
-prev_pot_value: int = pot_value + 1
-print("initial pot_value: " + str(pot_value) +
+pot_value: float = pin0_read_analog(0)
+pot_value_int = round_up(pot_value)
+prev_pot_value: float = pot_value + diff_fuzz;
+
+print("initial pot_value: " + str(pot_value) + " " + str(pot_value_int) +
       " prev_pot_value: " + str(prev_pot_value))
 
 ## Loop updating pin1 if pin0 changed we do this
 counter: int = 0
 while True:
     counter = counter + 1
-    if prev_pot_value != pot_value:
-        # It's different so update pin1 and tell the user
-        pin1.write_analog(pot_value)
-        print("counter: " + str(counter) + " pot_value: " + str(pot_value))
-        print_readings()
+    diff: float = abs(prev_pot_value - pot_value)
 
-        # Update prev_pot_value so we can detect changes
-        prev_pot_value = pot_value
+    # If the difference is greater than the diff_fuzz or
+    # the pot_value_int is 0 and diff is >= diff_clamp_to_0
+    # then we clamp at 0 otherwise sometimes we won't zero.
+    if (diff >= diff_fuzz) or ((pot_value_int == 0) and (diff >= diff_clamp_to_0)):
+        # It's different so update pin1 and tell the user
+        pin1.write_analog(pot_value_int)
+        print("counter: " + str(counter) +
+              " pot_value: " + str(pot_value) + " " + str(pot_value_int) +
+              " prev_pot_value: " + str(prev_pot_value) +
+              " diff: " + str(diff))
 
     # Read new value, may or may not be different
-    pot_value = pin0_read_analog()
+    prev_pot_value = pot_value
+    pot_value = pin0_read_analog(prev_pot_value)
+    pot_value_int = round_up(pot_value)
+
+    # This slight delay improves the smoothness around 0
+    # not sure why but it does.
+    utime.sleep_ms(50)

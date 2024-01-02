bb44d792bca3684fde20e9d7e4f35bd2ae5b747c
Wink Saville <wink@saville.com>
2024-01-02 13:58:58 -0800

    feat: Various improvements
    1) Print info every 3000ms
    2) Changed clamping so it's based on pot_value being close to zero, I did
         this because I noticed sometimes the pot wouldn't go to zero so triggering
         when it's zero won't work all the time. Also, not zero_threashold is 0.8.
         Initiall I had it at 0.5 but sometimes it didn't "fire" using 0.8 seems to work
         all the time.
    3) Add a print_info routine to reduce repetition and give consistenet information
    4) Change the delay at the end of the loop to potentially be adjustable and skip
         sleep_ms if the loop_delay_ms is 0.
    5) Add various funtions to support the changes.

--- 14.1_SoftLight/SoftLight.py
+++ 14.1_SoftLight/SoftLight.py
@@ -16,44 +16,77 @@ def round_up(x: float) -> int:
     """Round up a float to the nearest integer"""
     return int(x + 0.5)
 
-# Trigger to update pin1
-diff_fuzz = 1.0
+def print_info(extra: str):
+        print("counter: " + str(counter) +
+              " pot_value: " + str(pot_value) + " " + str(pot_value_int) +
+              " prev_pot_value: " + str(prev_pot_value) +
+              " diff: " + str(diff) +
+              " " + extra)
 
-# Clamp pot_value to 0 if diff is >= diff_clamp_to_0
-diff_clamp_to_0 = 0.01
+def update_next_info_time():
+    global next_info_time
+    next_info_time = utime.ticks_add(utime.ticks_ms(), info_threshold)
+
+def is_next_info_time() -> bool:
+    if utime.ticks_diff(next_info_time, utime.ticks_ms()) <= 0:
+        update_next_info_time()
+        return True
+    else:
+        return False
+
+# Trigger to update pin1
+diff_fuzz: float = 1.0
+
+# Zero threshold
+zero_threshold: float = 0.8
 
 # Read pot_value and make prev_pot_value different
 # so the first time through the loop we'll always
 # write a new value.
 pot_value: float = pin0_read_analog(0)
 pot_value_int = round_up(pot_value)
-prev_pot_value: float = pot_value + diff_fuzz;
+prev_pot_value: float = pot_value + (diff_fuzz * 2);
+diff: float = 0.0
+counter: int = 0
+info_threshold: int = 3000
+next_info_time: int = update_next_info_time()
+loop_delay_ms: int = 10
 
-print("initial pot_value: " + str(pot_value) + " " + str(pot_value_int) +
-      " prev_pot_value: " + str(prev_pot_value))
+print_info("Initial")
 
 ## Loop updating pin1 if pin0 changed we do this
-counter: int = 0
 while True:
     counter = counter + 1
     diff: float = abs(prev_pot_value - pot_value)
 
+    # Every few seconds output the info
+    if is_next_info_time():
+        print_info("info @ " + str(utime.ticks_ms()))
+
+    # The pot can be flaky and not actually get to zero
+    # so we'll fake if it's below zero_threshold.
+    if (pot_value < zero_threshold) and (prev_pot_value != 0):
+        pin1.write_analog(0)
+        print_info("Clamp to 0")
+
+        # Set previous value to 0 as we've forced it
+        prev_pot_value = 0
+
     # If the difference is greater than the diff_fuzz or
-    # the pot_value_int is 0 and diff is >= diff_clamp_to_0
-    # then we clamp at 0 otherwise sometimes we won't zero.
-    if (diff >= diff_fuzz) or ((pot_value_int == 0) and (diff >= diff_clamp_to_0)):
-        # It's different so update pin1 and tell the user
+    if (diff > diff_fuzz):
         pin1.write_analog(pot_value_int)
-        print("counter: " + str(counter) +
-              " pot_value: " + str(pot_value) + " " + str(pot_value_int) +
-              " prev_pot_value: " + str(prev_pot_value) +
-              " diff: " + str(diff))
+        print_info("diff > diff_fuzz")
+
+        # Save as previous value
+        prev_pot_value = pot_value
 
     # Read new value, may or may not be different
-    prev_pot_value = pot_value
     pot_value = pin0_read_analog(prev_pot_value)
     pot_value_int = round_up(pot_value)
 
-    # This slight delay improves the smoothness around 0
-    # not sure why but it does.
-    utime.sleep_ms(50)
+    # Previously this made things "smoother", not sure why,
+    # but now maybe this might reduce power consumption, at
+    # least that's possible. Of course more delay make it less
+    # responsive.
+    if loop_delay_ms > 0:
+        utime.sleep_ms(loop_delay_ms)
